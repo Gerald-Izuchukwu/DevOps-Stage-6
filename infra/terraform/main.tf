ea0b53fe7405de88a47f652b2ea54e23bf2ee36a
Gerald_Izuchukwu <geraldlouisugwunna@gmail.com>
2025-12-01 23:00:50 +0100

    test

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -60,6 +60,14 @@ resource "aws_security_group" "todo_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+
+  ingress {
+    from_port   = 3600
+    to_port     = 3600
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
